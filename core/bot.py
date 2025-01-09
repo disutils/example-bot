5fef283c8207ca5de3e4b3fecccd5a0c47ede0f8
Jiggly Balls <jigglyballs9000@gmail.com>
2025-01-09 22:22:57 +0530

    Fixed typing issues

--- core/bot.py
+++ core/bot.py
@@ -1,9 +1,17 @@
 from discord.ext import commands
 
+# from disckit.utils.translator import LemmaTranslator
+from typing import Any
+
 
 class Bot(commands.Bot):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, **kwargs: Any) -> None:
         super().__init__(command_prefix=None, **kwargs)
+
+    async def setup_hook(self) -> None:
+        ...
+        # await self.tree.set_translator(LemmaTranslator(self))
+        # await LemmaTranslator.setup(self, self.tree)
 
     async def on_ready(self) -> None:
         print(f"\n{self.user.name} is Online!\n")
